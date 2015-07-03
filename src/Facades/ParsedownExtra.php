316d6f295ffd867aaefa74b55049c911e7f678e9
Alfredo Ramos <AlfredoRamos@users.noreply.github.com>
2015-07-03 02:10:27 -0500

    Moved namespace to the top

--- src/Facades/ParsedownExtra.php
+++ src/Facades/ParsedownExtra.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace AlfredoRamos\ParsedownExtra\Facades;
 /**
  * Copyright (C) 2015 Alfredo Ramos
  *
@@ -15,8 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-namespace AlfredoRamos\ParsedownExtra\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
