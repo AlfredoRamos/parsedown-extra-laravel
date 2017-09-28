486705e892ac72449f0784e44926981716fb1b4a
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-09-28 04:25:20 -0500

    Change PHPDocBlock header

--- tests/Facades/ParsedownExtraTest.php
+++ tests/Facades/ParsedownExtraTest.php
@@ -1,20 +1,12 @@
 <?php
 
 /**
- * Copyright (C) 2015 Alfredo Ramos
+ * Parsedown Extra package for Laravel
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2015 Alfredo Ramos
+ * @license GNU GPL-3.0+
+ * @link https://github.com/AlfredoRamos/parsedown-extra-laravel
  */
 
 namespace AlfredoRamos\Tests\Facades;
