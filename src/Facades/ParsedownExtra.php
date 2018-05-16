47ddaa2aab5b10e0809dddd0484dcad1a397f912
Alfredo Ramos <alfredo.ramos@yandex.com>
2018-05-16 13:42:47 -0500

    Update license name
    Identifier GPL-3.0+ has been deprecated in favor of GPL-3.0-or-later
    
    https://spdx.org/licenses/GPL-3.0+.html
    https://spdx.org/licenses/GPL-3.0-or-later.html

--- src/Facades/ParsedownExtra.php
+++ src/Facades/ParsedownExtra.php
@@ -5,7 +5,7 @@
  *
  * @author Alfredo Ramos <alfredo.ramos@yandex.com>
  * @copyright 2015 Alfredo Ramos
- * @license GPL-3.0+
+ * @license GPL-3.0-or-later
  * @link https://github.com/AlfredoRamos/parsedown-extra-laravel
  */
 
