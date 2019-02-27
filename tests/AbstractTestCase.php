b9c302233b26bb0be87a9206a32e4a7a8d58874a
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-02-26 21:27:22 -0600

    Prepare for Laravel 5.8.x

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -21,7 +21,7 @@ abstract class AbstractTestCase extends TestCase {
 	 *
 	 * @return void
 	 */
-	protected function setUp() {
+	protected function setUp(): void {
 		parent::setUp();
 
 		$this->artisan('vendor:publish', [
